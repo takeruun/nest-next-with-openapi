5e5a15882473b8ae94928bb346bca32f5fb2d5b0
takeru.hashimoto <hstake25@gmail.com>
2022-11-21 09:04:30 +0900

    open api swagger

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+  ApiCreatedResponse,
+} from '@nestjs/swagger';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +13,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOkResponse({
+    description: 'Hello API',
+  })
+  @ApiUnauthorizedResponse({
+    description: '認証エラー',
+  })
+  @HttpCode(200)
   getHello(): string {
     return this.appService.getHello();
   }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { dump } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Swagger拡張の有効化
+  const config = new DocumentBuilder()
+    .setTitle('API description')
+    .setVersion('1.0')
+    .addServer('/')
+    .addCookieAuth() // swaggerのauthorizeを有効化
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // YAML
+  fs.writeFileSync('./openapi/openapi.yaml', dump(document, {}));
+
+  await app.listen(3010);
 }
 bootstrap();
